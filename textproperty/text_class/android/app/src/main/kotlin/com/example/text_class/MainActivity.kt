7f009da3b25032b70dda26cb5ffc25af355caa16
surukalamkar <fluttercareer45@gmail.com>
2024-02-21 11:19:26 +0530

    text and theme

---
+++ textproperty/text_class/android/app/src/main/kotlin/com/example/text_class/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.text_class
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ theme/themeappbar/android/app/src/main/kotlin/com/example/themeappbar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.themeappbar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
